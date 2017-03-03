20abb63f27d3676c64ee5ab7fa77bbf4070d3439
李晓光 <lxg@ziteng.cn>
2017-03-03 10:13:50 +0800

    Simple Test Demo ，毫无意义的demo，但是便于理解

--- app/src/main/java/com/tj/chaersi/rxjavademo/SimpleActivity.java
+++ app/src/main/java/com/tj/chaersi/rxjavademo/SimpleActivity.java
@@ -7,22 +7,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Single;
+import rx.Subscriber;
 import rx.functions.Action1;
 
 public class SimpleActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button simpleBtn;
+    private Button simpleBtn,SimpleBtn2;
+    private LinearLayout parentLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
 
+        parentLayout= (LinearLayout) findViewById(R.id.parentLayout);
+        SimpleBtn2= (Button) findViewById(R.id.SimpleBtn2);
+        SimpleBtn2.setOnClickListener(this);
         simpleBtn= (Button) findViewById(R.id.SimpleBtn);
         simpleBtn.setOnClickListener(this);
 
@@ -31,16 +37,42 @@ public class SimpleActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    int[] imgRes = new int[]{
+            R.mipmap.emoji_1f44f, R.mipmap.emoji_1f48e, R.mipmap.emoji_1f48f, R.mipmap.emoji_1f49a,
+            R.mipmap.emoji_1f49b, R.mipmap.emoji_1f49c, R.mipmap.emoji_1f49e, R.mipmap.emoji_1f60b,
+    };
+
     ArrayList<String> resArr=new ArrayList<>();
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.SimpleBtn:
+                Observable.from(resArr).subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i("simple",s);
+                    }
+                });
 
-        Observable.from(resArr).subscribe(new Action1<String>() {
-            @Override
-            public void call(String s) {
-                Log.i("simple",s);
-            }
-        });
-
+                break;
+            case R.id.SimpleBtn2:
+                Observable.create(new Observable.OnSubscribe<Integer>() {
+                    @Override
+                    public void call(Subscriber<? super Integer> subscriber) {
+                        for(Integer res:imgRes){
+                            subscriber.onNext(res);
+                        }
+                        subscriber.onCompleted();
+                    }
+                }).subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer ints) {
+                        ImageView subView=new ImageView(SimpleActivity.this);
+                        subView.setImageResource(ints);
+                        parentLayout.addView(subView);
+                    }
+                });
+                break;
+        }
     }
 }
